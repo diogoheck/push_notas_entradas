7540ce1a63008d3fbb35da303d8979cd57dd9c2a
diogoheck <diogo.heck.dev@gmail.com>
2023-03-13 15:38:22 -0300

    versao_9

--- app.py
+++ app.py
@@ -13,8 +13,6 @@ def limpar_conteudo_pasta():
     for arquivo in os.listdir('R:\\Compartilhado\\Push\\xmls_temp'):
         if os.path.isfile('R:\\Compartilhado\\Push\\xmls_temp' + os.sep + arquivo):
             os.remove('R:\\Compartilhado\\Push\\xmls_temp' + os.sep + arquivo)
-            
-
 
 
 if __name__ == '__main__':
@@ -23,45 +21,46 @@ if __name__ == '__main__':
     #     os.remove(LOCAL_GRAVAR)
     # if os.path.exists(LOCAL_GRAVAR_DUPL):
     #     os.remove(LOCAL_GRAVAR_DUPL)
-    
 
     # Criar as pastas dos clientes para armazenar os xmls
     while 1:
-        
-        for cnpj, credencias in dic_credencais_clientes.items():
-            
-            cwd = os.getcwd()
-            arquivos = os.listdir('R:/Compartilhado/Push/xmls_temp/Resumo_xml')
-            for arquivo in arquivos:
-                os.chdir('R:/Compartilhado/Push/xmls_temp/Resumo_xml')
-                os.remove(arquivo)
-            os.chdir(cwd)
-            caminho_padrao = f'R:/Compartilhado/Push/'
-            limpar_conteudo_pasta()
+        try:
+            for cnpj, credencias in dic_credencais_clientes.items():
 
-            caminho_pasta = caminho_padrao  + credencias.get('NOME_PASTA')
-            # print(caminho_pasta)
-    
-            # se a pasta nao existe, crie a pasta
-            if not os.path.isdir(caminho_pasta):
-                os.mkdir(caminho_pasta)
+                cwd = os.getcwd()
+                arquivos = os.listdir(
+                    'R:/Compartilhado/Push/xmls_temp/Resumo_xml')
+                for arquivo in arquivos:
+                    os.chdir('R:/Compartilhado/Push/xmls_temp/Resumo_xml')
+                    os.remove(arquivo)
+                os.chdir(cwd)
+                caminho_padrao = f'R:/Compartilhado/Push/'
+                limpar_conteudo_pasta()
 
-            # se nao ha nenhum xml na pasta, faça a requisicao
-            # if not os.listdir(caminho_pasta):
-            # salvar_logs('*' * 50, LOCAL_GRAVAR)
-            # salvar_logs(credencias.get('NOME_PASTA'), LOCAL_GRAVAR)
-            # salvar_logs('*' * 50, LOCAL_GRAVAR)
+                caminho_pasta = caminho_padrao + credencias.get('NOME_PASTA')
+                # print(caminho_pasta)
 
-            print('*' * 50)
-            print(credencias.get('NOME_PASTA'))
-            print('*' * 50)
-            print(credencias)
-            # fazer as requisições / downloads
-            requisicao_web(credencias)
-            renomear_xml(credencias.get('NOME_PASTA'))
+                # se a pasta nao existe, crie a pasta
+                if not os.path.isdir(caminho_pasta):
+                    os.mkdir(caminho_pasta)
 
-            
+                # se nao ha nenhum xml na pasta, faça a requisicao
+                # if not os.listdir(caminho_pasta):
+                # salvar_logs('*' * 50, LOCAL_GRAVAR)
+                # salvar_logs(credencias.get('NOME_PASTA'), LOCAL_GRAVAR)
+                # salvar_logs('*' * 50, LOCAL_GRAVAR)
+
+                print('*' * 50)
+                print(credencias.get('NOME_PASTA'))
+                print('*' * 50)
+                print(credencias)
+                # fazer as requisições / downloads
+                requisicao_web(credencias)
+                renomear_xml(credencias.get('NOME_PASTA'))
+        except:
+            pass
+
         print('*' * 50)
         print('aguardando 1 hora')
         print('*' * 50)
-        sleep(4000)
+        sleep(3600)
